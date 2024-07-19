ebe8a3f86b70247a6793a779763a11e66afbf202
gwon428 <152463087+gwon428@users.noreply.github.com>
2024-07-19 23:49:20 +0900

    [level 0] Title: 세 개의 구분자, Time: 112.87 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String[] solution(String myStr) {
+        myStr = myStr.replaceAll("[a|b|c]+", ",");
+        myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+        myStr = myStr.equals("") ? "EMPTY" : myStr;
+        return myStr.split("[,]");
+    }
+}
